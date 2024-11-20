bb0570ca9b1bec577a82a953f7d8828863b5f7ab
benhtodd <benhtodd@gmail.com>
2024-11-19 16:54:07 -0800

    update

---
+++ portainer/portainer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker run -d \
+  -p 8000:8000 \
+  -p 9443:9443 \
+  --name portainer \
+  --restart=always \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /home/toddb/portainer/data:/data \
+  portainer/portainer-ce:latest
